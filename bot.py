58458e80e4127bf435cbca52eae169bd94903b3a
Gr3ta <37002870+Gr3ta@users.noreply.github.com>
2018-03-06 18:23:43 +0200

    1+reaction credit: Doucer

--- bot.py
+++ bot.py
@@ -88,7 +88,9 @@ async def on_message(message):
             await bot.command('help', )
         else:
             await bot.send_message(channel, f"""Sun is shining, birds are chirping, flowers are blooming. On the days like this, kids like you, {message.author.mention}, should be burning in Hell...""")
-
+    elif text == "<_>":
+        await bot.send_message(channel, ">_<")
+        
     else:
         await bot.process_commands(message)
 
