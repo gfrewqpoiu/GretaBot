b7b94a8b090548167401e04a27657347de5b9e94
Kevin <Kevin.Striek@gmail.com>
2018-12-21 19:06:40 +0100

    Add help to upcoming command

--- bot.py
+++ bot.py
@@ -308,6 +308,7 @@ async def changes(ctx):
 
 @bot.command()
 async def upcoming(ctx):
+    """Previews upcoming plans if there are any"""
     await ctx.send("""This is upcoming:```I am rn in the process of being rewritten and updated to Python 3.7
     I heard gfrew is doing that because then I will automatically reconnect to Discord,
     if there are any connection issues. So I will be on here more often.```""")
