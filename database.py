924be16a41b12574d226e0eaa77148c71facb47c
Kevin <Kevin.Striek@gmail.com>
2019-01-04 21:07:42 +0100

    Add Quote System

---
+++ database.py
@@ -0,0 +1,16 @@
+from peewee import *
+from playhouse.sqliteq import SqliteQueueDatabase
+
+db = SqliteQueueDatabase("bot.db")
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+class Quote(BaseModel):
+    guildId = IntegerField()
+    keyword = CharField()
+    result = TextField(null=False)
+    authorId = IntegerField(null=False)
+
+Quote.add_index(Quote.guildId, Quote.keyword)
