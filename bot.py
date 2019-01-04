589d30d0934f78eacb174bd743a05e85a9fc668c
Gr3ta <37002870+Gr3ta@users.noreply.github.com>
2019-01-04 13:09:27 +0200

    MOAR QUESTIONS
    Added just a tad bit of more of questions

--- bot.py
+++ bot.py
@@ -113,7 +113,7 @@ async def on_message(message):
             if inputcheck(tripped.clean_content.lower()):
                 await channel.send(f"Okay use the {bot.command_prefix}help command to get a list of my commands!")
             elif inputcheck(tripped.clean_content.lower()) == False:
-                await channel.send(f"""Oh my fucking GOD! Fuck you {message.author.mention}! >:c""")
+                await channel.send(f"""Oh my love... Then maybe don't ping me, {message.author.mention}? ;/""")
 
         else:
             pass
@@ -133,8 +133,15 @@ async def on_message(message):
         if text == "how are you?":
             await channel.send("I am fine.")
         elif text == "what are you doing?":
-            await channel.send("Look at my playing status, duh.")
-
+            await channel.send("Look at my playing status.")
+        elif text == "where I can find rules?":
+            await channel.send("Rules are in #rules_and_rules_updates, have a nice day :D.")
+        elif text == "where I can post my artworks/book?":
+            await channel.send("You can post your artwork in #art_corner and your book in #book_promotes :D.")
+        elif text == "where I can dump my memes and shitpost?":
+            await channel.send("Meme dumpage happens in #dank_meme_depository and shitposting in #shitposting :D.")
+        elif text == "how are you even responding?":
+            await channel.send("My master did her magic... :eyes: ") 
     else:
         await bot.process_commands(message)
 
