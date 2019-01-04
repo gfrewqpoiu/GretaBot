6be1d6e06f92b3b600b05bbf0b7fa0a34e99a1e4
Kevin <Kevin.Striek@gmail.com>
2019-01-04 21:50:48 +0100

    Fix delquote command

--- bot.py
+++ bot.py
@@ -464,7 +464,7 @@ async def addquote(ctx, keyword: str, *, quotetext: str):
 async def delquote(ctx, keyword: str):
     quote = Quote.get_or_none(Quote.guildId == ctx.guild.id, Quote.keyword == keyword.lower())
     if quote:
-        quote.delete()
+        quote.delete_instance()
         await ctx.send("The quote was deleted.")
     else:
         await ctx.send("I could not find the quote.")
