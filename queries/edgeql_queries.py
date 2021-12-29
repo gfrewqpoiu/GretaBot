c70d2a90fa485066c0328f17377c54c3bd7d8ccd
Kevin Striek <Kevin.Striek@gmail.com>
2021-12-29 10:46:50 +0000

    Lint edgeql_queries.py

--- queries/edgeql_queries.py
+++ queries/edgeql_queries.py
@@ -4,9 +4,14 @@ import discord
 from typing import Union, Optional, Final, cast
 
 instance_name: Final[str] = "SAIL"
-AsyncQuerySource = Union[edgedb.AsyncIOConnection, edgedb.AsyncIOTransaction, edgedb.AsyncIOPool]
+AsyncQuerySource = Union[
+    edgedb.AsyncIOConnection, edgedb.AsyncIOTransaction, edgedb.AsyncIOPool
+]
 
-async def _run_query(query: str, source: AsyncQuerySource, *args, **kwargs) -> edgedb.Set:
+
+async def _run_query(
+    query: str, source: AsyncQuerySource, *args, **kwargs
+) -> edgedb.Set:
     if not isinstance(source, edgedb.AsyncIOTransaction):
         async for tx in source.retrying_transaction():
             async with tx:
